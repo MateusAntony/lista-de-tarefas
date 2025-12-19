00fdb40bd9def67f13bd880ac9c1840f31a7660f
MateusAntony <mateus.medeiros110@gmail.com>
2025-12-19 09:41:39 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,137 @@
-import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button"
+import { Card,CardHeader,CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Plus,Check,List,SquareX,Pencil,Trash,ListChecks,Sigma  } from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+const Home = () => {
+  return(
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center" >
+      
+      
+      <Card className="w-lg">
+        <CardHeader className= "flex gap-2">
+          <Input placeholder="Adicionar tarefa"/>
+          <Button  className= "cursor-pointer"> <Plus />Cadastrar</Button>
+        </CardHeader>
+
+
+        <CardContent>
+          <Separator className="mb-4"/>
+          <div className="flex gap-2">
+
+            <Badge  className="cursor-pointer" variant="default">  <List /> Todos</Badge>
+            <Badge  className="cursor-pointer" variant="outline"> <SquareX/> Não finalizados</Badge>
+            <Badge  className="cursor-pointer" variant="outline"> <Check/> Concluidas</Badge>
+
+          </div>
+
+          <div className="mt-4 border-b-1">
+
+            <div className="flex justify-between h-14 items-center border-t-1">
+                <div className="w-2 h-full bg-green-300"></div>
+                <p className="flex-1 px-2 text-sm">Tarefa 1</p>
+
+                <div className="flex gap-2 items-center">
+
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Pencil size={16} className="cursor-pointer"/>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Editar tarefa</DialogTitle>
+                      </DialogHeader>
+
+                      <div className="flex gap-2">
+
+                        <Input placeholder="Editar da tarefa"/>
+                        <Button className="cursor-poiter"> Salvar</Button>
+                      </div>
+
+                    </DialogContent>
+                  </Dialog>
+
+                  <Trash size={16} className="cursor-pointer"/>
+                </div>  
+            </div>
+
+            <div className="flex justify-between h-14 items-center border-t-1">
+                <div className="w-2 h-full bg-green-300"></div>
+                <p className="flex-1 px-2 text-sm">Tarefa 1</p>
+
+                <div className="flex gap-2 items-center">
+                  <Pencil size={16} className="cursor-pointer"/>
+                  <Trash size={16} className="cursor-pointer"/>
+                </div>  
+            </div>
+          </div>
+
+
+          <div className="flex gap-2 justify-between mt-4">
+
+            <div className="flex gap-2 items-center">
+              <ListChecks size={18} />
+              <p className="text-xs">Tarefas concluidas (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="outline" className="cursor-pointer text-xs h-7"><Trash size={16} /> Limpar tarefas concluidas</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Tem certeza que deseja excluir x itens?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div className="h-2  bg-blue-500 rounded-md" style={{ width: '50%' }}></div>
+          </div>
+
+
+          <div className="flex justify-end items-center gap-2 mt-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tarefas no total</p>
+          </div>
+
+
+        </CardContent>
+
+        <div></div>
+
+
+      </Card>
+    </main>
+  )
 }
+
+export default Home;
